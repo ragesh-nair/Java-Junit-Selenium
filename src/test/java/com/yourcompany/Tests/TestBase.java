7d5d465757230e29018b77ebaa010957a461dce5
Ragesh Nair <ragesh.nair@rakuten.com>
2017-11-14 12:01:12 +0900

    Missed the function call

--- src/test/java/com/yourcompany/Tests/TestBase.java
+++ src/test/java/com/yourcompany/Tests/TestBase.java
@@ -162,7 +162,7 @@ public class TestBase implements SauceOnDemandSessionIdProvider {
     @BeforeClass
     public static void setupClass() {
         //get the uri to send the commands to.
-        seleniumURI = "@ondemand.saucelabs.com:443";
+        seleniumURI = buildSauceUri();
         //If available add build tag. When running under Jenkins BUILD_TAG is automatically set.
         //You can set this manually on manual runs.
         buildTag = System.getenv("BUILD_TAG");
